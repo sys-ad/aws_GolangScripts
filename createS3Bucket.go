9b9c1af37695083253eeab9cedc7a9e87e2b3891
MoGo <95191519+sys-ad@users.noreply.github.com>
2021-12-13 18:41:28 -0500

    Update createS3Bucket.go

--- createS3Bucket.go
+++ createS3Bucket.go
@@ -9,3 +9,25 @@ import (
   
          "fmt"
 )
+
+func main() {
+        sess, err :=session.NewSessionWithOptions(session.Options{
+                Profile: "default"
+                Config: aws.Config{
+                        Region: aws.String{"us-east-1"),
+                },
+        })
+         
+        if err != nil {
+                fmt.Printf("Failed to initialize new session: %v", err)
+                return
+        }
+                
+                uploader := s3manager.NewUploader(sess)
+                
+                result, err := uploader.Upload(&s3manager.UploadInput{
+                    Bucket: aws.String(myBucket),
+                    Key:    aws.String(myString),
+                    Body:    
+                
+                                
