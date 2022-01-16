45b5eb56fb7f14c24bc6f2dba8c53829165483fe
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-16 13:25:17 -0500

    Update uploadObjectS3.go

--- uploadObjectS3.go
+++ uploadObjectS3.go
@@ -31,28 +31,27 @@ func main() {
         filename := os.Args[1]
         key      := os.Args[2]
                            
-                           file, err := os.Open(filename)
-                           if err != nil {
-                                   log.Fatalf("os.Open - filename %v, err: %v", filename, err)
-                           }
-                           defer file.Close()
-                           
-                           
-                           sess, err :=session.NewSession(&aws.Config{Region: aws.String(S3_REGION))}
-                           if err != nil {
-                                   log.FatalF("session.NewSession - filename: %v, err: %v", filename, err)
-                           }
+        file, err := os.Open(filename)
+        if err != nil {
+                log.Fatalf("os.Open - filename %v, err: %v", filename, err)
+        }
+        defer file.Close()
+                                 
+        sess, err :=session.NewSession(&aws.Config{Region: aws.String(S3_REGION))}
+        if err != nil {
+                log.FatalF("session.NewSession - filename: %v, err: %v", filename, err)
+        }
                                                           
-                           handler := S3Handler {
-                               v   Session: sess,
-                                   Bucket: S3_BUCKET,
-                           }
+        handler := S3Handler {
+                Session: sess,
+                Bucket: S3_BUCKET,
+        }
                            
-                           err = handler.UploadFile(key, filename)
-                           if err != nil {       
-                                   log.FatalF("UploadFile - filename: %v, err: %v", filaname, err)
-                           }
-                           log.Info("UploadFile - success")
+        err = handler.UploadFile(key, filename)
+        if err != nil {       
+                log.FatalF("UploadFile - filename: %v, err: %v", filaname, err)
+        }
+        log.Info("UploadFile - success")
                            
 }
                                                           
@@ -112,31 +111,4 @@ func (h S3Handler) ReadFile(key string) (string, error
                                                            
                                                            
                                                           
-         
-        if err != nil {
-                fmt.Printf("Failed to initialize new session: %v", err)
-                return
-                }
-        }        
-                uploader := s3manager.NewUploader(sess)
-                f, err := os.Open(somepdf.pdf) // stream mode??
-                if err != nil {
-                    return fmt.Errorf("failed to open file %q
-                
-                result, err := uploader.Upload(&s3manager.UploadInput{
-                    Bucket: aws.String(myBucket),
-                    Key:    aws.String(myString),
-                    Body:   f,
-        })
-                                      if err != nil {
-                                              return.Errorf("failed to upload file, %v", err)
-                                      }
-                                      fmt.Printf("file uploaded to, %s\n", aws.Stringvalue(result.Location))
-                       // to be continued... 12/13/21'
-// -----------------------------------------------------------------------------------------
         
-                                      
-  func uploadDataToS3(dir string, svc *s3.S3) {
-      fileList := []string{}
-      filepath.          
-                                
