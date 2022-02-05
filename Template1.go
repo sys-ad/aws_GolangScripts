bb29e88ba46f4c8ff1ce10c8600f29932392ac7e
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-02-05 00:01:14 -0500

    Update Template1.go

--- Template1.go
+++ Template1.go
@@ -8,3 +8,12 @@ import (
         
         "github.com/aws/aws-sdk-go-v2/config" // this package loads configuration
         "github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+var (
+        bucketName      string 
+        objectPrefix    string
+        objectDelimiter string
+        maxKeys         int
+)
+        
