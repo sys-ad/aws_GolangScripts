8aaed5d600522fbc5e8768a7f3804c7f43e4d0fb
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-02-05 17:01:37 -0500

    Update create_Instance.go

--- create_Instance.go
+++ create_Instance.go
@@ -5,20 +5,25 @@ import (
   ""
   "fmt"
   "github.com/aws/aws-sdk-go"
+  "github.com/aws/aws-lambda-go/lambda"
+  "github.com/session"
 )
-AMI := ''
-INSTANCE_TYPE := ''
-REGION := ''
-KEY_NAME := ''
 
 func main() {
-  
+  svc := ec2.New(session.New())
+  input := &ec2.RunInstancesInput{
+    ImageId:      aws.String(AMI),
+    InstanceType: aws.String(INSTANCE_TYPE),
+    KeyName:      aws.String(KEY_NAME),
+    MaxCount:     aws.Int64(1),
+    MinCount:     aws.Int64(1),
+ }          
   
 }
 
-func LambdaHandler(ctx context, name event) (string, error) {
-        return 
-  
+func Lambda_Handler(ctx context, name event) (string, error) {
+     return 
+     
 }
 
 
