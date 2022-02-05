8c0bf31cf43c989b8f914f122e1b8b04c3694d03
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-02-04 23:48:09 -0500

    Update Template1.go

--- Template1.go
+++ Template1.go
@@ -1,4 +1,10 @@
 package main
 
 import (
-        "context"
+        "context"  //
+        "flag" //
+        "fmt"
+        "log"
+        
+        "github.com/aws/aws-sdk-go-v2/config"
+        "github.com/aws/aws-sdk-go-v2/service/s3"
